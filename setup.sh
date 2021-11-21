10d23cc397402a61d98fb22f0c881bc71a730c34
Dr. Chaojie Wang <wcj365@gmail.com>
2021-11-20 19:41:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/bash
 
 # Step 1
-# Install required Python packages
-
+# Install required Python packages for Jupyter Book
 pip install -r requirements.txt
 
+# Step 2 Update apt-get
 sudo apt-get update
 
-# Step 2 
-# Install 楷体 font for Chinese Character
+# Step 3 
+# Install 楷体 font for Chinese Character  "AR PL UKai TW"
 # https://gist.github.com/allex/11203573
-
 sudo apt-get install fonts-arphic-ukai
 
-# Step 3
-# Install pandoc
+# Step 4
+# Install latex-cjk-all  to support Chinese/Jaopanese/Korean language for LaTex 
+sudo apt install latex-cjk-all`
 
+
+# Step 5
+# install LaTex engine (texlive) and LaTex to PDF transformation library (XeLaTex) required for Pandoc
+# texlive is a LaTex engine and xetex is a Latex to PDF transformation enginer which supports UTF-8 unicode for Chinese characters
+sudo apt install texlive-xetex
+
+# Step 6
+# Install pandoc
 sudo apt-get install pandoc
