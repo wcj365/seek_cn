5d4fb85ddd0a46158ad2a7138269c4f9a449a699
Dr. Chaojie Wang <wcj365@gmail.com>
2021-12-11 18:27:21 +0000

    Built the static website of the book.

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 #1 Step 1 build static website using Jupyter Book
 
-cd jupyter_book
-
 . jupyterbook.sh 
 
-# Step 2 build pdf and epub version of the book
 
-cd pandoc
+# Step 2 build pdf and epub version of the book
 
 . pandoc.sh 
