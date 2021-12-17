a34776f9561a1c597e02a7fc6771ecb907e1d186
Dr. Chaojie Wang <wcj365@gmail.com>
2021-12-17 21:54:30 +0000

    Built the PDF verion of the book.

--- pandoc.sh
+++ pandoc.sh
@@ -6,8 +6,8 @@ python pandoc.py
 
 # Step 2. Generate PDF version of the book
 
-pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` -o docs/offline/dao_de_jing.pdf
-pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` -o docs/offline/dao_de_jing.epub
+pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` --toc-depth=2 -o docs/offline/dao_de_jing.pdf
+pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` --toc-depth=2 -o docs/offline/dao_de_jing.epub
 
 # Step 3. Push the changes to GitHub
 
